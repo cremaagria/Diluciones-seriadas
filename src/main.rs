581f3efe63827f6ef976981dc9ae9281ca60fd86
Andres Zaraza <zarazafelipe@gmail.com>
2025-09-21 21:27:00 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,24 @@ fn main() {
     let r = calcular_r(&x, &y);
 
     println!("Las muestras diluidas son: \n{:?}", x);
-
     println!("El r de la muestra es: {} \nEl r^2 es: {}", r, r.powi(2));
+    
 }
 
+//Funciones estdisticas-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Calculas la media de un sett de datos de valor flotante.
+fn media(datos: &[f64]) -> f64 {
+    datos.iter().sum::<f64>() / datos.len() as f64
+}
+
+// Calcula la desviacion estandar usando apartir de la media y el set de datos.
+fn devest(datos: &[f64], media: f64) -> f64 {
+    let sum: f64 = datos.iter().map(|&dato| (dato - media).powi(2)).sum();
+    (sum / (datos.len() as f64 - 1.0)).sqrt()
+}
+
+// Calcula el r usando las funciones para calcular la media y la desviacion estandar 
 fn calcular_r(x: &[f64], y: &[f64]) -> f64 {
     let mx = media(x);
     let my = media(y);
@@ -24,15 +38,9 @@ fn calcular_r(x: &[f64], y: &[f64]) -> f64 {
     sum / (x.len() as f64 - 1.0)
 }
 
-fn media(datos: &[f64]) -> f64 {
-    datos.iter().sum::<f64>() / datos.len() as f64
-}
+//Funciones de laboratorio----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-fn devest(datos: &[f64], media: f64) -> f64 {
-    let sum: f64 = datos.iter().map(|&dato| (dato - media).powi(2)).sum();
-    (sum / (datos.len() as f64 - 1.0)).sqrt()
-}
-
+// Calcula la concentracion de la muestras despues de un numero de diluciones con determinado factor de dilucion y una concentración inicial
 fn diluir(muestra: f64, factor: f64, pasos: usize) -> Vec<f64> {
     std::iter::successors(Some(muestra), move |&prev| Some(prev / factor))
         .take(pasos)
